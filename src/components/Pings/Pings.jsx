f05fefe0f5a39d4d029e34a24f2e5415535c9aeb
NovaDev <nova@labs.dev>
2026-07-19 16:54:58 -0700

    gave Pings appendages

--- src/components/Pings/Pings.jsx
+++ src/components/Pings/Pings.jsx
@@ -3,7 +3,8 @@ import "./Pings.css";
 function Pings() {
     return (
         <div className="pings"> 
-            <div className="head">              
+            <div className="head">
+                <div className="neck"></div>              
                 <div className="screen">
                     <div className="eye-left"></div>
                     <div className="eye-right"></div>
@@ -15,14 +16,19 @@ function Pings() {
             </div>
             <div className="antenna-right">
                 <div className="antenna-tip"></div>
-            </div>
-                </div> {/* head ending */}
-                
-                
-        
-
-        
-        </div> 
+            </div>  
+            <div className="chest">
+                <div className="chest-plate">
+                    <p>B-001
+                        </p>
+                    </div>
+            </div> {/* chest ending*/}
+                <div className="left-arm-socket"></div>
+                <div className="right-arm-socket"></div>
+                <div className="belly"></div>
+                <div className="hips"></div>
+        </div>
+    </div>
     );
 }
 export default Pings;
